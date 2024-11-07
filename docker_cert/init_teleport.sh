9ed57ded85c963bd5a06b17206924ec86c2e5357
jphaugla <jasonhaugland@gmail.com>
2024-11-07 09:53:49 -0600

    default is openssl build

--- docker_cert/init_teleport.sh
+++ docker_cert/init_teleport.sh
@@ -3,6 +3,6 @@ docker-compose build
 # build keys using cockroach cert
 # ./build_keys.sh
 # build keys using openssl
-# ./build_openssl_keys.sh
+./build_openssl_keys.sh
 # builds the psql image used to test psql connectivity issues
 docker-compose -f docker-compose-psql.yml build
